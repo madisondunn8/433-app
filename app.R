c80027a941e88184800cc8e7b03fa1099827fbe6
Madison Dunn <medunn3@wisc.edu>
2021-12-15 14:51:34 -0600

    app

--- app.R
+++ app.R
@@ -5,6 +5,7 @@ installed_packages <- packages %in% rownames(installed.packages())
 if (any(installed_packages == FALSE)) {
   install.packages(packages[!installed_packages])
 }
+invisible(lapply(packages, library, character.only = TRUE))
 
 library(shiny)
 library(tidyverse)
